d58f62410117e11129922ea386af68a7aea49cb1
vogel2 <aalmonzer@gmail.com>
2020-10-16 01:18:20 +0200

    first commit

---
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pawns/PawnBase.h"
+#include "PawnTank.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API APawnTank : public APawnBase
+{
+	GENERATED_BODY()
+	
+};
